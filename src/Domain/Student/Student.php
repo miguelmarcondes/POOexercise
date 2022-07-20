b719c096eb799295e5f4073fe383b0963aa7f1e6
miguelmarcondes <miguelfmarcondes@gmail.com>
2022-07-20 20:33:17 -0300

    refactoring of student and videos classes

--- src/Domain/Student/Student.php
+++ src/Domain/Student/Student.php
@@ -66,19 +66,16 @@ class Student
 
     public function hasAccess(): bool
     {
-        if ($this->watchedVideos->count() > 0) {
-            $this->watchedVideos->sort(fn (DateTimeInterface $dateA, DateTimeInterface $dateB) => $dateA <=> $dateB);
-            /** @var DateTimeInterface $firstDate */
-            $firstDate = $this->watchedVideos->first()->value;
-            $today = new \DateTimeImmutable();
-
-            if ($firstDate->diff($today)->days >= 90) {
-                return false;
-            } else {
-                return true;
-            }
-        } else {
-            return true;
+        if ($this->watchedVideos->count() === 0) {
+            return true;     
         }
+
+        $this->watchedVideos->sort(fn (DateTimeInterface $dateA, DateTimeInterface $dateB) => $dateA <=> $dateB);
+        /** @var DateTimeInterface $firstDate */
+        $firstDate = $this->watchedVideos->first()->value;
+        $today = new \DateTimeImmutable();
+
+        return $firstDate->diff($today)->days < 90;; 
     }
+
 }
